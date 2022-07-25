4bcf9cd2861b5a7d558786fed3face6b4febef4a
roaaab23-meet <roaa.ab86@gmail.com>
2022-07-25 18:20:03 +0300

    frontend review

---
+++ main.js
@@ -0,0 +1,11 @@
+function changeBackgroundColor(color){
+	if(document.getElementById("w").style.backgroundColor==color){
+		document.getElementById("w").style.backgroundColor='red';
+	}
+	else{
+	document.getElementById("w").style.backgroundColor=color;
+	}
+	
+	
+
+}
